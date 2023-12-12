ef33fbd4a07fdf19368257974b2fe150960b8183
inesal <79632240+inesal@users.noreply.github.com>
2023-12-12 17:16:49 +0100

    Create Jeu.cs

---
+++ Jeu.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Threading;
+using System.Runtime.Serialization.Json;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace ProjetAlgoMotGliss
+{
+    internal class Jeu
+    {
+        private Dictionnaire dictionnaire;
+        private Plateau plateau;
+        private List<Joueur> joueurs;
+        private System.Timers.Timer timerJeu;
+        private int tempsTour; // Temps par tour en secondes
+        private int joueurActuelIndex;
+        private bool jeuEnCours;
+        private Thread chronometre;
+
+        public Jeu(string cheminDictionnaire, Plateau plateau , int tempsTour = 30 )
+        {
+            dictionnaire = new Dictionnaire(cheminDictionnaire);
+            this.plateau = plateau; // Taille du plateau par défaut, peut être ajustée
+            joueurs = new List<Joueur>();
+            this.tempsTour = tempsTour;
+            jeuEnCours = false;
+
+            timerJeu = new System.Timers.Timer(tempsTour * 1000);
+            timerJeu.Elapsed += OnTourTermine; // temps ecoulé
+        }
+
+        public void AjouterJoueur(string nom)
+        {
+            joueurs.Add(new Joueur(nom));
+        }
+
+        public void DemarrerJeu()
+        {
+            if (joueurs.Count < 2)
+            {
+                Console.WriteLine("Nombre insuffisant de joueurs.");
+                return;
+            }
+
+            jeuEnCours = true;
+            joueurActuelIndex = 0;
+
+            chronometre = new Thread(ChronometreThread);
+            
+            chronometre.Start();
+            LancerTour();
+        }
+
+        private void ChronometreThread(Thread chronometre )
+        {
+            while (jeuEnCours)
+            {
+                Console.Write($"\rTemps restant : {tempsTour} secondes ");
+                Thread.Sleep(1000); // Attendre 1 seconde
+                tempsTour--;
+
+
+                if (tempsTour <= 0)
+                {
+                    // Si le temps est écoulé, arrêter le tour
+                    OnTourTermine(null, null);
+                    chronometre.Stop();
+                }
+                
+            }
+        }
+
+        private void LancerTour()
+        {
+            if (!jeuEnCours) return;
+
+            Console.WriteLine($"C'est le tour de {joueurs[joueurActuelIndex].Nom}. Vous avez {tempsTour} secondes.");
+
+            // Afficher l'état actuel du plateau
+            Console.WriteLine("État actuel du plateau :");
+            Console.WriteLine(plateau.ToString());
+
+            timerJeu.Start();
+        }
+
+
+        private void OnTourTermine(Object timer, ElapsedEventArgs timersource)
+        {
+            timerJeu.Stop();
+            Console.WriteLine($"Temps écoulé pour {joueurs[joueurActuelIndex].Nom}!");
+
+            joueurActuelIndex = (joueurActuelIndex + 1) % joueurs.Count;
+
+            LancerTour();
+        }
+
+        public void SoumettreMot(string mot)
+        {
+            if (!jeuEnCours)
+            {
+                Console.WriteLine("Le jeu n'est pas en cours.");
+                return;
+            }
+
+            Joueur joueurActuel = joueurs[joueurActuelIndex];
+
+            if (plateau.Recherche_Mot(mot) && dictionnaire.RechDichoRecursif(mot))
+            {
+
+                if (!joueurActuel.Contient(mot))
+                {
+
+                    joueurActuel.Add_Mot(mot);
+                    joueurActuel.Add_Score(joueurActuel.CalculerScoreMot (mot));
+                    plateau = new Plateau("PlateauFinal.csv");
+                    Console.WriteLine(joueurActuel.ToString());
+
+                    // Mettre à jour le score ici en fonction de votre logique de calcul de score
+
+                    Console.WriteLine(" \n Mot accepté, continuez");
+                }
+                else
+                {
+                    Console.WriteLine("Mot déjà trouvé par ce joueur.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Mot non valide ou non présent sur le plateau.");
+            }
+        }
+    }
+}
