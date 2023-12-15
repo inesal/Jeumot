842f88fe1a25071dd7d61f1ac4ef2b75e5ce6700
inesal <79632240+inesal@users.noreply.github.com>
2023-12-15 02:31:51 +0100

    ///

--- Joueur.cs
+++ Joueur.cs
@@ -28,6 +28,11 @@ namespace ProjetAlgoMotGliss
         }
 
         //constructeur
+
+        /// <summary>
+        /// Constricteur de la classe joueur avec un nom donné
+        /// </summary>
+        /// <param name="nom">Le nom du joueur</param>
         public Joueur(string nom)
         {
             if (nom != null && nom.Length != 0)
@@ -41,6 +46,10 @@ namespace ProjetAlgoMotGliss
                 Console.WriteLine("Le joueur n'a pas de nom");
             }
         }
+        /// <summary>
+        /// Ajoute un mot à la liste des mots trouvés du joueur s'il n'est pas déjà présent
+        /// </summary>
+        /// <param name="mot">Le mot à ajouter</param>
         public void Add_Mot(string mot)
         {
             if (!Contient(mot))
@@ -51,13 +60,17 @@ namespace ProjetAlgoMotGliss
             }
         }
 
-
+        /// <summary>
+        /// Calcule le score d'un mot en utilisant un ensemble de valeurs attribuées à chaque lettre
+        /// </summary>
+        /// <param name="mot">Le mot pour lequel calculer le score</param>
+        /// <returns>Le score du mot</returns>
         public int CalculerScoreMot(string mot)
         {
             
             Dictionary<char, int> val = new Dictionary<char, int>
         {
-            {'A', 1 }, {'B', 2} , {'C' , 4} , {'D' , 5} , { 'E' ,  6} , {'F' , 9} , { 'G' , 2} , {'H' , 1} , {'I' , 3} , {'J' , 1} , {'K', 3} , { 'L' , 2 } , {'M' , 2 } , {'O' , 6} , {'P' , 8} , {'Q' , 2} ,{ 'R' , 2} , { 'S' , 4} , {'T' , 6} , { 'U' , 1},{'V' , 2} , {'W' , 10} , {'X' , 1} , { 'Y' , 7} , {'Z' , 10}
+            {'A', 1 }, {'B', 2} , {'C' , 4} , {'D' , 5} , { 'E' ,  6} , {'F' , 9} , { 'G' , 2} , {'H' , 1} , {'I' , 3} , {'J' , 1} , {'K', 3} , { 'L' , 2 } , {'M' , 2 } ,{'N',1 }, {'O' , 6} , {'P' , 8} , {'Q' , 2} ,{ 'R' , 2} , { 'S' , 4} , {'T' , 6} , { 'U' , 1},{'V' , 2} , {'W' , 10} , {'X' , 1} , { 'Y' , 7} , {'Z' , 10}
 
         };
             int scoreMot = 0;
@@ -70,20 +83,27 @@ namespace ProjetAlgoMotGliss
                 {
                     // Ajouter le score de la lettre au score total du mot
                     scoreMot += val.GetValueOrDefault(lettre);
-                    Console.WriteLine(scoreMot);
+                    //Console.WriteLine(scoreMot);
                 }
                 // Vous pouvez également choisir de gérer le cas où la lettre n'est pas présente dans le dictionnaire
             }
             return scoreMot;
             
         }
-
+        /// <summary>
+        /// Ajoute un score spécifié au score existant du joueur
+        /// </summary>
+        /// <param name="val">La valeur à ajouter au score</param>
         public void Add_Score( int val)
         {
             score += val;
 
         }
-        
+        /// <summary>
+        /// Vérifie si le joueur a déjà utilisé un mot
+        /// </summary>
+        /// <param name="mot">Le mot à vérifier</param>
+        /// <returns>True si le mot a déjà été utilisé sinon False</returns>
         public bool Contient(string mot)
         {
             foreach (string element in motsTrouves)
@@ -96,7 +116,10 @@ namespace ProjetAlgoMotGliss
             }
             return false;
         }
-
+        /// <summary>
+        /// Retourne les informations du joueur
+        /// </summary>
+        /// <returns>Une chaîne de caractères</returns>
         public override string ToString()
         {
             string result = "";
